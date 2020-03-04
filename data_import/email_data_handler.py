4a584f70c7e94ffacb534325af8c2553da5755e0
Restyled.io <commits@restyled.io>
2020-03-04 13:05:11 +0000

    Restyled by yapf

--- data_import/email_data_handler.py
+++ data_import/email_data_handler.py
@@ -36,8 +36,8 @@ class EmailDataHandler(DataHandler):
                             for col in self.__columns:
                                 if line.startswith(col):
                                     row[self.__columns.index(col)] = (
-                                        line.strip().replace(":", "").replace(col, "")
-                                    )
+                                        line.strip().replace(":", "").replace(
+                                            col, ""))
                                     continue
 
                             if line.startswith(self.__last_line):
@@ -50,17 +50,8 @@ class EmailDataHandler(DataHandler):
                 rows.append(row + [content])
 
         df = pd.DataFrame(rows, columns=self.__columns + ["Content"])
-        df["combined"] = (
-            df["From"].astype(str)
-            + " "
-            + df["To"]
-            + " "
-            + df["Cc"]
-            + " "
-            + df["Subject"]
-            + " "
-            + df["Content"]
-        )
+        df["combined"] = (df["From"].astype(str) + " " + df["To"] + " " +
+                          df["Cc"] + " " + df["Subject"] + " " + df["Content"])
         self.df = df.fillna("")
 
     def display_labels(self):
