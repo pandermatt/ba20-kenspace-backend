dd87d51d469a0be4a791038583a79d7de4aa08e3
Pascal Andermatt <p.andermatt@me.com>
2020-03-20 20:45:37 +0100

    Add logger

--- util/logger.py
+++ util/logger.py
@@ -3,9 +3,13 @@ import logging
 
 def get_logger():
     logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        format='%(asctime)s %(threadName)-12.12s %(levelname)-8s %(message)s',
         level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S')
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.FileHandler("app.log"),
+            logging.StreamHandler()
+        ])
     return logging.getLogger(__name__)
 
 
