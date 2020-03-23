f55b84d72be482604fed961dd652b6c0a52818de
Pascal Andermatt <p.andermatt@me.com>
2020-03-23 15:05:37 +0100

    Add log to file upload

--- file_io/file_upload.py
+++ file_io/file_upload.py
@@ -4,13 +4,17 @@ import pandas as pd
 from werkzeug.datastructures import FileStorage
 
 from config import config
+from util.logger import log
 
 
 def handle_file_upload(file_storage: FileStorage):
     file_uuid = f'{str(uuid.uuid4())}.csv'
+
+    log.info(f'Save file {file_uuid}')
     filename = config.custom_input_file(file_uuid)
     file_storage.save(filename)
 
+    log.info(f'Extract CSV headers')
     data = pd.read_csv(filename)
 
     return {'cols': [col for col in data.columns], 'filename': file_uuid}
