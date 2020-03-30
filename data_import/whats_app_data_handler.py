a84fcf09f953449eba6478a49c0217d2f6d40706
Pascal Andermatt <p.andermatt@me.com>
2020-03-30 16:49:30 +0200

    Anonym Whatsapp

--- data_import/whats_app_data_handler.py
+++ data_import/whats_app_data_handler.py
@@ -1,9 +1,10 @@
+import random
+
 import pandas as pd
+from faker import Faker
 
 from config import config
 from data_import.data_handler import DataHandler
-from util.text_clean_up import clean_up_text
-from util.timed_cache import timed_cache
 
 
 class WhatsAppDataHandler(DataHandler):
@@ -13,7 +14,11 @@ class WhatsAppDataHandler(DataHandler):
         DataHandler.__init__(self, 'WhatsApp')
         input_data = config.input_data_file('_chat.txt')
 
+        fake = Faker(['de_DE', 'en_US'])
+
         rows = []
+        # name_map = {}
+        names = [fake.first_name() for _ in range(10)]
         with open(input_data) as fp:
             while True:
                 row = [''] * 2
@@ -22,7 +27,16 @@ class WhatsAppDataHandler(DataHandler):
                 if not line:
                     break
                 line = ''.join(line.split("]")[1:]).strip()
-                row[0] = line.split(":")[0]
+
+                if len(line.split(":")) < 2:
+                    continue
+
+                # name = line.split(":")[0]
+                # if name not in name_map:
+                #     name_map[name] = fake.first_name()
+
+                # row[0] = name_map[name]
+                row[0] = random.choice(names)
                 row[1] = ''.join(line.split(":")[1:]).strip()
 
                 rows.append(row)
@@ -30,16 +44,16 @@ class WhatsAppDataHandler(DataHandler):
         df = pd.DataFrame(rows, columns=self.__columns)
         df['combined'] = df['From'].astype(str) + ': ' + df['Content']
         self.df = df.fillna('')
+        self.saved_item_to_cluster = self.clean_up_df_text('combined', language="german")
 
     def display_labels(self):
-        return self.df['combined'].tolist()
+        return self.df['Content'].tolist()
 
-    @timed_cache(minutes=30)
-    def __cached_cleanup(self, col):
-        return clean_up_text(self.df, col)
+    def meta_info(self):
+        return [{"content": f'from {content}'} for content in self.df['From'].tolist()]
 
-    def item_to_cluster(self):
-        return self.__cached_cleanup('combined')
+    def calculate_n_clusters(self):
+        return 1000
 
 
 if __name__ == '__main__':
