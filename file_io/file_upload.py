a0b84dc7ca7c4739668f0776ba983f9b9698bed3
Pascal Andermatt <p.andermatt@me.com>
2020-04-06 15:41:11 +0200

    adapt upload handler

--- file_io/file_upload.py
+++ file_io/file_upload.py
@@ -9,15 +9,23 @@ from werkzeug.datastructures import FileStorage
 from api.errors import error_response
 from config import config
 from data_import.data_handler import calculate_n_clusters_by_category
+from file_io.directory_analyzer import DirectoryAnalytics
 from util.logger import log
+
+CSV_FILE = 'csv'
+TXT_FILE = 'txt'
+WHATS_APP_TXT_FILE = 'whatsapp'
+ZIP = 'zip'
 
 
 def handle_file_upload(file_storage: FileStorage, upload_type: str):
-    file_uuid = f'{str(uuid.uuid4())}_{file_storage.filename}.csv'
+    filename, file_extension = os.path.splitext(file_storage.filename)
+    file_uuid = f'{str(uuid.uuid4())}_{filename}'
 
     log.info(f'Save file {file_uuid}')
-    filename = config.custom_input_file(file_uuid)
-    file_storage.save(filename)
+    original_filename = config.custom_input_file(f'{file_uuid}{file_extension}')
+    csv_filename = config.custom_input_file(f'{file_uuid}.csv')
+    file_storage.save(original_filename)
 
     log.info(f'Extract {upload_type}')
 
@@ -25,22 +33,29 @@ def handle_file_upload(file_storage: FileStorage, upload_type: str):
     detected_language = None
 
     try:
-        if upload_type == 'csv':
-            data = pd.read_csv(filename)
-        elif upload_type == 'txt':
-            data = convert_txt_to_csv(filename)
-        elif upload_type == 'whatsapp':
-            data = convert_whats_app_to_csv(filename)
+        if upload_type == CSV_FILE:
+            data = pd.read_csv(original_filename)
+        elif upload_type == TXT_FILE:
+            data = convert_txt_to_csv(original_filename)
+        elif upload_type == WHATS_APP_TXT_FILE:
+            data = convert_whats_app_to_csv(original_filename)
+        elif upload_type == ZIP:
+            data = DirectoryAnalytics(original_filename, [CSV_FILE, TXT_FILE]).pandas_data
         else:
-            error_response(f"Not supported yet")
+            error_response(f"{file_extension} not supported yet")
 
         sample_text = " ".join(map(str, data.iloc[0].tolist()))
         detected_language = iso639.to_name(detect(sample_text)).lower()
     except Exception as e:
-        log.error(f'Remove file due to an error: {filename}')
-        os.remove(filename)
+        log.error(f'Remove file due to an error: {original_filename}')
+        os.remove(original_filename)
         log.error('An exception occurred: %r', e)
         error_response(f"Wrong data format")
+
+    if upload_type != CSV_FILE:
+        log.info(f'Remove file: {original_filename}')
+        os.remove(original_filename)
+        data.to_csv(csv_filename)
 
     return {
         'cols': [col for col in data.columns],
@@ -62,12 +77,7 @@ def convert_txt_to_csv(filename):
             if striped_line and striped_line != '':
                 rows.append(striped_line)
 
-    data = pd.DataFrame(rows, columns=['text-content'])
-
-    # Override file
-    data.to_csv(filename)
-
-    return data
+    return pd.DataFrame(rows, columns=['text-content'])
 
 
 def convert_whats_app_to_csv(filename):
@@ -90,8 +100,5 @@ def convert_whats_app_to_csv(filename):
     data = pd.DataFrame(rows, columns=columns)
     data['Combined'] = data['From'].astype(str) + ': ' + data['Content']
     data.fillna('')
-
-    # Override file
-    data.to_csv(filename)
 
     return data
